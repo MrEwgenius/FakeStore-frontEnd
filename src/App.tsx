76ef9f8cbf6566e28c2885d32f530a4c8d213fc9
Bolynskii Ewgenii <119881162+MrEwgenius@users.noreply.github.com>
2024-11-07 14:13:22 +0300

    eddit app setinterval

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,29 @@ import Router from "./pages/Router";
 import { getBrandProduct } from "./redux/reducers/productSlice";
 
 function App() {
-  const dispatch = useDispatch();
-  setInterval(() => {
-    dispatch(getBrandProduct());
-  }, 10000);
+
+  const url = `https://fakestore-backend-ozwi.onrender.com/`;
+  const interval = 15000; // Interval in milliseconds (30 seconds)
+ 
+  function reloadWebsite() {
+    fetch(url)
+      .then((response) => {
+        console.log(
+          `Reloaded at : Status Code ${
+            response
+          }`
+        );
+      })
+      .catch((error) => {
+        console.error(
+          `Error reloading at ${new Date().toISOString()}:`,
+          error.message
+        );
+      });
+  }
+
+  setInterval(reloadWebsite, interval);
+
   return (
     <div className="App">
       <Router />
