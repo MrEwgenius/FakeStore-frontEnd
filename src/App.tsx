117c2c76f9844f0514715fd84be01d06b75d4e92
Bolynskii Ewgenii <119881162+MrEwgenius@users.noreply.github.com>
2024-11-07 13:18:45 +0300

    eddit app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useDispatch } from "react-redux";
 import Router from "./pages/Router";
+import { getBrandProduct } from "./redux/reducers/productSlice";
 
 function App() {
-    return (
-        <div className="App">
-            <Router />
-        </div>
-    );
+  const dispatch = useDispatch();
+  setInterval(() => {
+    dispatch(getBrandProduct());
+  }, 10000);
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
 }
 
 export default App;
